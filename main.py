20475cb2746825e19f160948d248236380c99f18
Justin <73510469+spanosjustin@users.noreply.github.com>
2024-01-03 21:17:46 -0500

    Add files via upload
    Updates:
    - Moved clock in and out features to their own methods outside of main loop and then referenced them in main loop,
    - Built out the employeeWorkWeekData dictionary to hold data better
    - Added the logic to add clock in and out times to employeeWorkWeekData dictionary
    - Added a method to determine length if shift and then store it in the employeeWorkWeekData dictionary

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ running = True
 # Get and format the currewnt date and time
 now = datetime.now()
 dateAndTime = now.strftime("%d/%m/%y %H:%M")
+shiftHour = 0
+shiftMinute = 0
+totalShift = 0
 
 # Intitializing Data Base
 employeeData = {
@@ -30,7 +33,20 @@ employeeData = {
 
 # For Later
 employeeWorkWeekData = {
-    123456: { "Empty"
+    123456: {
+        "in":{},
+        "out":{},
+        "shifts":{}
+        },
+    654321: {
+        "in":{},
+        "out":{},
+        "shifts":{}
+        },
+    111111: {
+        "in":{},
+        "out":{},
+        "shifts":{}
         }
     }
 
@@ -39,10 +55,79 @@ def invalidFunction():
     print("Invalid Number")
 
 def clockIn():
-    print()
+    global employeeInUse
+    global employeeData
+    global employeeWorkWeekData
+    currentTime = datetime.now()
+    timeIn = currentTime.strftime("%d/%m/%y %H:%M")
+    if employeeInUse in employeeData:
+        employeeData[employeeInUse]["isClockedIn"] = True
+        print()
+        print("==================================")
+        print("           Clocked In")
+        print("        ", employeeData[employeeInUse]["name"])
+        print("   Clocked in at:", dateAndTime)
+        print("==================================")
+        print()
+        for x in range(len(employeeWorkWeekData[employeeInUse]["in"]) + 1):
+            if(x >= len(employeeWorkWeekData[employeeInUse]["in"])):
+                employeeWorkWeekData[employeeInUse]["in"][x] = timeIn
 
 def clockOut():
-    print()
+    global employeeInUse
+    global employeeData
+    global employeeWorkWeekData
+    currentTime = datetime.now()
+    timeIn = currentTime.strftime("%d/%m/%y %H:%M")
+    if employeeInUse in employeeData:
+        employeeData[employeeInUse]["isClockedIn"] = False
+        recentShift = len(employeeWorkWeekData[employeeInUse]["shifts"])
+        print()
+        print("==================================")
+        print("           Clocked Out")
+        print("        ", employeeData[employeeInUse]["name"])
+        print("   Clocked out at:", dateAndTime)
+        print("==================================")
+        print()
+        for x in range(len(employeeWorkWeekData[employeeInUse]["out"]) + 1):
+            if(x >= len(employeeWorkWeekData[employeeInUse]["out"])):
+                employeeWorkWeekData[employeeInUse]["out"][x] = timeIn
+
+def shiftLength():
+    global employeeInUse
+    global employeeWorkWeekData
+    global shiftMinute
+    global shiftHour
+    # Variables
+    clockInTime = str(employeeWorkWeekData[employeeInUse]["in"])
+    clockOutTime = str(employeeWorkWeekData[employeeInUse]["out"])
+    
+    clockInHour = int(clockInTime[14:16])
+    clockInMin = int(clockInTime[17:19])
+    clockOutHour = int(clockOutTime[14:16])
+    clockOutMin = int(clockOutTime[17:19])
+    
+    # Logic for hours
+    if(clockOutHour >= clockInHour):
+        shiftHour = clockOutHour - clockInHour
+    else:
+        clockOutHour += 24
+        shiftHour = clockOutHour - clockInHour
+    # Logic for minutes
+    if(clockOutMin >= clockInMin):
+        shiftMinute = clockOutMin - clockInMin
+    else:
+        clockOutMin += 60
+        shiftMinute = clockOutMin - clockInMin
+    # Combine Minutes and hours
+    shiftMinute = round((shiftMinute / 60), 2)
+    totalShift = shiftHour + shiftMinute
+    for x in range(len(employeeWorkWeekData[employeeInUse]["shifts"]) + 1):
+        if(x >= len(employeeWorkWeekData[employeeInUse]["shifts"])):
+            employeeWorkWeekData[employeeInUse]["shifts"][x] = totalShift
+            employeeWorkWeekData[employeeInUse]["shifts"][x]
+    print(employeeWorkWeekData[employeeInUse]["shifts"][len(employeeWorkWeekData[employeeInUse]["shifts"])-1])
+            
 
 while(running == True):
     employeeInUse = int(input("Enter Numbers: "))
@@ -52,23 +137,7 @@ while(running == True):
         invalidFunction()
     # Check if the number is the correct number
     if(employeeData[employeeInUse]["isClockedIn"] == False):
-        if employeeInUse in employeeData:
-            employeeData[employeeInUse]["isClockedIn"] = True
-            print()
-            print("==================================")
-            print("           Clocked In")
-            print("        ", employeeData[employeeInUse]["name"])
-            print("   Clocked in at:", dateAndTime)
-            print("==================================")
-            print()
+        clockIn()
             
     elif(employeeData[employeeInUse]["isClockedIn"] == True):
-            employeeData[employeeInUse]["isClockedIn"] = False
-            print()
-            print("==================================")
-            print("           Clocked Out")
-            print("        ", employeeData[employeeInUse]["name"])
-            print("   Clocked out at:", dateAndTime)
-            print("==================================")
-            print()
-
+        clockOut()
