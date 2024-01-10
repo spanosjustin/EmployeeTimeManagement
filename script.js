a18f51d1706fbf2adf9f7804d370dbd07466f751
Justin <73510469+spanosjustin@users.noreply.github.com>
2024-01-09 22:46:22 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 // script.js
+
+// declare date variables
+let currentDate = newDate();
+
+let hour = currentDate.getHours();
+let minute = currentDate.getMinutes();
+
+// declare other variables
+let testEmployeeNumber = 111111;
+
+let numPadInput;
+
+
+
+
+
+
 function printHelloWorld() {
     var outputElement = document.getElementById("output");
 
     outputElement.innerHTML = "Hello World";
+}
+
+/*
+ *
+ *  Working on // Logic Test
+ *
+ */
+
+function printNumber(number) {
+    // Get the output element by its ID
+    var outputElement = document.getElementById("output");
+
+    // Update the content with the clicked number
+    outputElement.textContent = "You clicked number " + number;
+}
+
+
+
+
+function collectemployeeID(){
+}
+
+function clockIn() {
+    if(userInput == testEmployeeNumber) {
+    }
 }
